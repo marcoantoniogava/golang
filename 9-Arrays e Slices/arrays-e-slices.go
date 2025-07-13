b646edce4f318eb4c256d80fe4d35dbdbb293989
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-07-13 16:11:54 -0300

    Arrays internos

--- 9-Arrays e Slices/arrays-e-slices.go
+++ 9-Arrays e Slices/arrays-e-slices.go
@@ -32,5 +32,23 @@ func main() {
 
 	array2[1] = "Posição Alterada"
 	fmt.Println(slice2)
-	
+
+	//Arrays internos
+	fmt.Println("-------------------")
+	slice3 := make([]float32, 10, 11) //make cria um slice com a quantidade de elementos e o tamanho máximo
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //tamanho
+	fmt.Println(cap(slice3)) //capacidade
+
+	slice3 = append(slice3, 5) //slice na capacidade maxima
+	slice3 = append(slice3, 6) //estoura o limite, cria um novo slice com o dobro da capacidade
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) //tamanho
+	fmt.Println(cap(slice3)) //capacidade
+
+	slice4 := make([]float32, 5) //se não especificar a capacidade, a capacidade será igual ao tamanho
+	fmt.Println(slice4)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
