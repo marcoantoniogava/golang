3b9267c23f4c215930ffd8a1224eb785570d2edb
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-07-20 23:34:33 -0300

    Canais com buffer

--- 18-Concorrência/4-Canais com buffer/canais-com-buffer.go
+++ 18-Concorrência/4-Canais com buffer/canais-com-buffer.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	canal := make(chan string, 2) //especifica que o canal tem uma capacidade de 2 mensagens
+	canal := make(chan string, 200) //especifica que o canal tem uma capacidade de 2 mensagens
 	canal <- "Olá Mundo!"
 	canal <- "Programando em Go!"
 
 	mensagem := <-canal
 	mensagem2 := <-canal
-	
+
 	fmt.Println(mensagem)
 	fmt.Println(mensagem2)
 }
