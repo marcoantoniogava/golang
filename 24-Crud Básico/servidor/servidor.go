bcf46c106b1ef31f6ebd3e94fe68a8dd739cfbf1
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-07-26 22:35:22 -0300

    Inserindo usuários no banco de dados

---
+++ 24-Crud Básico/servidor/servidor.go
@@ -0,0 +1,61 @@
+package servidor
+
+import (
+	"crud/banco"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type usuario struct {
+	ID    uint32 `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json:"email"`
+}
+
+// CriarUsuario insere um usuário no banco de dados
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		w.Write([]byte("Falha ao ler o corpo da requisição!"))
+		return
+	}
+
+	var usuario usuario
+	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		w.Write([]byte("Erro ao converter o usuário para struct"))
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		w.Write([]byte("Erro ao conectar ao banco de dados!"))
+	}
+	defer db.Close()
+
+	statement, erro := db.Prepare("insert into usuarios (nome, email) values (?, ?)")
+	if erro != nil {
+		w.Write([]byte("Erro ao criar o statement!"))
+		return
+	}
+	defer statement.Close()
+
+	insercao, erro := statement.Exec(usuario.Nome, usuario.Email)
+	if erro != nil {
+		w.Write([]byte("Erro ao executar o statement!"))
+		return
+	}
+
+	idInserido, erro := insercao.LastInsertId()
+	if erro != nil {
+		w.Write([]byte("Erro ao obter o ID inserido!"))
+		return
+	}
+
+	//STATUS CODES
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(fmt.Sprintf("Usuário inserido com sucesso! id: %d", idInserido)))
+
+}
