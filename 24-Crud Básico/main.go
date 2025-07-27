36841287af200278a2b95b1d85d0cfaf9996650d
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-07-26 21:56:16 -0300

    Criando estrutura para CRUD

---
+++ 24-Crud Básico/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	//CRUD - CREATE, READ, UPDATE, DELETE
+
+	router := mux.NewRouter()
+
+	fmt.Println("Escutando na porta 5000")
+	log.Fatal(http.ListenAndServe(":5000", router))
+	
+}
